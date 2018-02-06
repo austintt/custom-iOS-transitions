1e2191ee680e2b74fc8cf8d0f5284bd075b03187
Austin Tooley <austin@tooley.me>
2018-02-05 19:46:24 -0600

    Initial Commit

---
+++ custom-iOS-transitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  custom-iOS-transitions
+//
+//  Created by Austin Tooley on 2/5/18.
+//  Copyright © 2018 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
