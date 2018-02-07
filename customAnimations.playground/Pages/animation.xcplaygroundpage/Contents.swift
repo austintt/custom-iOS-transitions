9cfe73baf22a0a284f2cbc93c70e5fc241549959
Austin Tooley <austin@tooley.me>
2018-02-06 20:26:24 -0600

    a couple more comments

--- customAnimations.playground/Pages/animation.xcplaygroundpage/Contents.swift
+++ customAnimations.playground/Pages/animation.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@
 import UIKit
 import PlaygroundSupport
 
+// MARK: Boring setup
 let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 600))
 containerView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 
@@ -12,7 +13,6 @@ func makeShape(color: UIColor, x: Int, y: Int, radius: CGFloat = 0) -> UIView{
     circle.backgroundColor = color
     return circle
 }
-
 
 let greenCircle = makeShape(color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), x: 20, y: 20, radius: 25)
 let blueSquare = makeShape(color: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1), x: 20, y: 100)
@@ -25,6 +25,7 @@ containerView.addSubview(redSquare)
 containerView.addSubview(pinkSquare)
 containerView.addSubview(purpleCircle)
 
+// MARK: Animations!
 UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse], animations: {
     
     /***
